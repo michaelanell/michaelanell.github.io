de3ce7d06d9fa09787d40820d2ee479f60c3262a
Michaela Nell <u718651165@byui.edu>
2021-01-16 20:02:06 -0700

    Update

---
+++ currentdate.js
@@ -0,0 +1,40 @@
+function date(){
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = month[d.getMonth()];
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
+document.getElementById("currentdate").textContent = fulldate; 
+}
+var currentdate = setInterval(date, onload);
+
+function lastUpdate(){
+    let update = document.lastModified;
+    document.getElementById("lastModified").innerHTML = " " + update;
+}
+
+var modified = setInterval(lastUpdate, onload);
