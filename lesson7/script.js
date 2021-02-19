fc83ef4bcadb03b40ef263a4fa75b20f506ea9c2
Use your GitHub username <Enter your email address>
2021-02-18 23:46:37 -0700

    Update

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,4 +1,20 @@
- if('IntersectionObserver' in window) {
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px, 0px, 300px, 0px"
+};
+
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+
+if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
         if(item.isIntersecting) {
