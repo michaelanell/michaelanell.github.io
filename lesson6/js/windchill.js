f8d213da3a7aac92b0e54cec122b146a7347ad7c
Use your GitHub username <Enter your email address>
2021-02-24 13:07:02 -0700

    Update

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,17 +1,30 @@
 
-function calculateWindChill(){
-    var temp = parseFloat(document.getElementById('temperature').innerHTML);
-    var windSpeed = parseFloat(document.getElementById('wind-Speed').innerHTML);
-    if (temp <= 10 && windSpeed >= 3) {
-        let r = windChill(temp, windSpeed);
-        document.getElementById('windChill').innerHTML = r; 
-    } else {
-        document.getElementById('windChill').innerHTML = "N/A"; 
+ /*function calculateWindChill(){ 
+
+    if (temp <= 50 && windSpeed >= 3) {
+        let r = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16);
+        document.getElementById('displayWindChill').innerHTML = Math.round(r).toFixed(0) + "℉";
+        } else {
+         r = "n/a";
+                        document.getElementById('WindChill').innerHTML = r;
+         }
+ } 
+ */
+
+document.getElementById("calculateWindChill").innerHTML = calculateWindChill(document.getElementById("wind-Speed").innerHTML, document.getElementById("temperature").innerHTML);
+
+ function calculateWindChill(temp, speed){
+    let r;
+    if ( temp <=50 && speed >3) {
+        r = 35.74 + (0.6215 * temp)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+
     }
+    else {
+        r = "N/A";  
+    }
+    return r.toFixed(2);
+
+    /* The toFixed() method converts a number into a string, rounding to a specified number of decimals. */
 }
-function windChill(tempF, speed){
-    let t = tempF;
-    let s = speed;
-    let f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
-    return f;
-}
+
+ 
