aa07d8b57b84acad15e61c64e6576afd490007d4
Use your GitHub username <Enter your email address>
2021-03-07 00:25:02 -0700

    Update

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -13,10 +13,12 @@ const cards = document.querySelector('.cards')
 towns.forEach(town => { if (town.name == "Preston" || town.name == "Soda Springs" || town.name == "Fish Haven"){
 // creates element - does not put it on the page, just creates it in memory
 let card = document.createElement('section');
+let div = document.createElement('div');
 let h2 = document.createElement('h2');
 let h3 = document.createElement('h3');
 let p = document.createElement('p');
 let img = document.createElement('img');
+
 
 // template literals
 h2.textContent = `${town.name}`;
@@ -24,11 +26,11 @@ h3.textContent = `${town.motto}`;
 p.textContent = `${town.yearFounded} ${town.currentPopulation} ${town.averageRainfall}`;
 img.setAttribute('src', `${town.photo}`);
 
-
-card.appendChild(h2);
-card.appendChild(h3);
-card.appendChild(p);
-card.appendChild(h3);
+card.appendChild(div);
+div.appendChild(h2);
+div.appendChild(h3);
+div.appendChild(p);
+div.appendChild(h3);
 card.appendChild(img);
 cards.appendChild(card);
 };
