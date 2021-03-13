342ea29d2705118161cc10c548625baa32245157
Use your GitHub username <Enter your email address>
2021-03-13 16:44:09 -0700

    Update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -19,6 +19,13 @@ const windSpeed = document.querySelector('#wind-Speed');
 windSpeed.textContent = jsObject.wind.speed;
 
 
+
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+//document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+
 //const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;  // note the concatenation
 //const desc = jsObject.weather[0].description;  // note how we reference the weather array
 //document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
