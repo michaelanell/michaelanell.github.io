8af25e2f09236316d8dc6c547c8d323247341099
Use your GitHub username <Enter your email address>
2021-03-20 21:48:40 -0600

    Update

--- lesson11/js/ssevents.js
+++ lesson11/js/ssevents.js
@@ -17,18 +17,27 @@ let event = document.createElement('section');
 let div = document.createElement('div');
 let h2 = document.createElement('h2');
 let h3 = document.createElement('h3');
-let content = document.createElement('p');
+let content1 = document.createElement('p');
+let content2 = document.createElement('p');
+let content3 = document.createElement('p');
+let content4 = document.createElement('p');
 
 // template literals
-h2.textContent = `${town.name}`;
+h2.textContent = town.name;
 h3.textContent = "Events";
-content.textContent = `${town.events} `;
+content1.textContent = town.events[0];
+content2.textContent = town.events[1];
+content3.textContent = town.events[2];
+content4.textContent = town.events[3];
 
 
 events.appendChild(div);
 div.appendChild(h2);
 div.appendChild(h3);
-div.appendChild(content);
+div.appendChild(content1);
+div.appendChild(content2);
+div.appendChild(content3);
+div.appendChild(content4);
 }; 
 
  });
